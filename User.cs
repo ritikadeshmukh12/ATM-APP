50e30ce2cad2e0e5c4afc874f72bae0823d123c6
ritikadeshmukh12 <118090878+ritikadeshmukh12@users.noreply.github.com>
2025-08-14 11:01:12 +0530

    Update User.cs

--- User.cs
+++ User.cs
@@ -1,1 +1,7 @@
+public class User {
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public decimal Balance { get; set; }
+}
 
