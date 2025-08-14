7820f7c179c38773e5d7f9aae72f2c970d30779c
ritikadeshmukh12 <118090878+ritikadeshmukh12@users.noreply.github.com>
2025-08-14 10:55:19 +0530

    Update AccountController.cs

--- AccountController.cs
+++ AccountController.cs
@@ -1,1 +1,21 @@
 
+public class AccountController : Controller {
+    private readonly ATMContext _context;
+
+    public AccountController(ATMContext context) {
+        _context = context;
+    }
+
+    public IActionResult Login() => View();
+
+    [HttpPost]
+    public IActionResult Login(string username, string password) {
+        var user = _context.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+        if (user != null) {
+            HttpContext.Session.SetInt32("UserId", user.Id);
+            return RedirectToAction("Index", "ATM");
+        }
+        ViewBag.Error = "Invalid credentials";
+        return View();
+    }
+}
