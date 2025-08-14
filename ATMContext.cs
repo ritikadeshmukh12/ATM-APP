6c647e5f76d8a52cab04069e59f4b19664eb8095
ritikadeshmukh12 <118090878+ritikadeshmukh12@users.noreply.github.com>
2025-08-14 11:13:15 +0530

    Update ATMContext.cs

--- ATMContext.cs
+++ ATMContext.cs
@@ -1,1 +1,7 @@
+public class ATMContext : DbContext {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    public ATMContext(DbContextOptions<ATMContext> options) : base(options) { }
+}
 
