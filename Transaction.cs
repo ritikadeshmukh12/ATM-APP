7baa612f1d32297bd0341958a413e2ab686df0f4
ritikadeshmukh12 <118090878+ritikadeshmukh12@users.noreply.github.com>
2025-08-14 11:11:51 +0530

    Update Transaction.cs

--- Transaction.cs
+++ Transaction.cs
@@ -1,1 +1,8 @@
+public class Transaction {
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public DateTime Date { get; set; }
+    public string Type { get; set; } // Deposit or Withdraw
+    public decimal Amount { get; set; }
+}
 
